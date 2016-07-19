70fe03e8e353a9f6e0dcb745a5f33f66934faa33
Shahriar Hossain <shahriar_cse@live.com>
2016-07-19 03:18:53 -0700

    Custom Config Added
    Retrieve  Messages from Custom Config

--- src/DotNetCoreDemo/Startup.cs
+++ src/DotNetCoreDemo/Startup.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DotNetCoreDemo
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            var buildConfig = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("customConfig.json");
+
+            Configuration = buildConfig.Build();
+        }
+
+        public IConfiguration Configuration { get; set; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -16,9 +27,11 @@ namespace DotNetCoreDemo
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
+            var responeGrettings = Configuration["grettings"];
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync(responeGrettings);
             });
         }
     }
