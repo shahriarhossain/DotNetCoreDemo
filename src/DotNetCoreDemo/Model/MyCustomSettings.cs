c03346901a3bd6cc5b631b0a14edbabd84508b44
Shahriar Hossain <shahriar_cse@live.com>
2016-07-19 07:59:38 -0700

    Strongly Typed Configuration
    Reading Data from configuration file

---
+++ src/DotNetCoreDemo/Model/MyCustomSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNetCoreDemo.Model
+{
+    public class MyCustomSettings
+    {
+        public string AuthKey { get; set; }
+        public string AppName { get; set; }
+        public int MaxItemPerList { get; set; }
+
+    }
+}
--- src/DotNetCoreDemo/Startup.cs
+++ src/DotNetCoreDemo/Startup.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using DotNetCoreDemo.Model;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace DotNetCoreDemo
 {
@@ -23,11 +25,18 @@ namespace DotNetCoreDemo
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddOptions();
+
+            //To avoid the error : Cannot convert from 'MicrosoftExtensions.Configuration.IConfigurationSection' to 'System.Action<T>' add a new dependency in json.
+            //http://stackoverflow.com/questions/37438230/asp-net-core-rc2-configure-custom-appsettings
+            services.Configure<MyCustomSettings>(Configuration.GetSection("MyCustomSettings"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, IOptions<MyCustomSettings> option)
         {
+            var appName = option.Value.AppName;
+           // var appName2 = Configuration.GetValue<string>("customConfig:AppName");
             var responeGrettings = Configuration["grettings"];
             var quotesOfTheDay = Configuration["customQuotes"];
 
